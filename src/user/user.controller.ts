9e8879378b4d76374cf0bebc63c8ca5154034c9e
maxcorp <shaparyuk@gmail.com>
2019-06-21 18:44:35 +0300

    added authorization

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Post, Get, Body, UsePipes, Query } from '@nestjs/common';
+import { Controller, Post, Get, Body, UsePipes, Query, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDTO } from './user.dto';
 import { pipe } from 'rxjs';
 import { ValidationPipe } from '../shared/validation.pipe';
+import { AuthGuard } from '../shared/auth.guard';
+import { User } from './user.decorator';
 
 @Controller()
 export class UserController {
     constructor(private userService: UserService) {}
 
     @Get('api/users')
-    showAllUsers() {
+    @UseGuards(new AuthGuard)
+    showAllUsers(@User() user) {
+        console.log(user);
         return this.userService.showAll();
     }
 
