37b24e924367861242fee5a509093946607f9f8f
maxcorp <shaparyuk@gmail.com>
2019-06-29 18:27:27 +0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ const port = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:['http://localhost:4200']
+  });
+
   await app.listen(port);
 
   Logger.log(`Server is running on http://localhost:${port}`, 'Bootstrap');
